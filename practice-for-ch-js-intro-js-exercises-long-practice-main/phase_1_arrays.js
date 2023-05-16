babf4c14c16b1b6d6c08df2c9b801ba8b2682cb3
Rocco Lattanzio <lukelattanzio@yahoo.com>
2023-05-16 14:07:24 -0700

    unique solved

--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase_1_arrays.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase_1_arrays.js
@@ -1,1 +1,12 @@
 // Your code here
+Array.prototype.unique = function() {
+    let arr = [];
+    for (let i = 0; i < this.length; i++) {
+        if (!arr.includes(this[i])) {
+        arr.push(this[i]);
+        };
+    };
+    return arr;
+};
+
+console.log[1,1,2,3].unique()
